3cf4a330cf961ce2837ce474354c00721f18a448
Carrie Schmid <carriepederson7@gmail.com>
2019-08-30 14:33:06 -0700

    Add .filter() method on array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-var newPizza = new Pizza();
+// var newPizza = new Pizza();
+//
+// function Pizza(){
+//   this.pizzaCost = []
+// }
 
-function Pizza(){
-  this.pizzaCost = []
-}
-
-function Toppings(base1, topping1, topping2, topping3, topping4){
+function Pizza(base1, topping1, topping2, topping3, topping4){
   this.base= parseInt(base1);
   this.pepperoni= parseInt(topping1);
   this.olives= parseInt(topping2);
@@ -18,17 +18,16 @@ function Toppings(base1, topping1, topping2, topping3, topping4){
 // }
 
 
-Pizza.prototype.Price = function(pizza){
-  this.pizzaCost.push(pizza);
-}
+// Pizza.prototype.Price = function(pizza){
+//   this.pizzaCost.push(pizza);
+// }
 
-Toppings.prototype.addToppings = function(){
+Pizza.prototype.addToppings = function(){
 this.base + this.pepperoni + this.olives + this.sausge + this.peppers
-
 }
 
-var newPizza = []
-console.log(newPizza);
+
+
 
 
 // UI Logic
@@ -54,14 +53,33 @@ var peppers = $("input:radio[name=peppers]:checked").val();
 
 
 var base = $("input:radio[name=start]:checked").val();
-console.log(base);
-console.log(pepperoni);
-console.log(sausage);
-var pizza1 = new Toppings(base, pepperoni, olives, sausage, peppers);
+// console.log(base);
+// console.log(pepperoni);
+// console.log(sausage);
+var pizza1 = new Pizza(base, pepperoni, olives, sausage, peppers);
 pizza1.addToppings(pizza1);
-newPizza.Price(pizza1)
+var prices = [pizza1.base.toString(), pizza1.pepperoni.toString(), pizza1.olives.toString(), pizza1.sausage.toString(), pizza1.peppers.toString()];
+var filtered = prices.filter(function(){
+return "10" || "2";
+
+});
+filtered
+prices
+console.log(filtered);
+// console.log(prices);
+
+
+// var result = []
+// // var nan = []
+// console.log(result);
+// console.log(prices);
+// for(var i =0; i<prices.length; i++){
+//   if([i] === "10"){
+//   result.push([i]);
+//   }
+// }
+
 // var pizzaPrice = []
-console.log(newPizza);
 // pizza1.forEach(function(pizza){
 //     if(pizza === 10 | 2){
 //       pizzaPrice.push(pizza)
