4895c3d182125c0b68e9b7a366ee77d54c7c4dae
Carrie Schmid <carriepederson7@gmail.com>
2019-09-08 20:18:11 -0700

    Delete comments from project.

--- js/scripts.js
+++ js/scripts.js
@@ -8,61 +8,24 @@ function Pizza(inputSize, toppings) {
 Pizza.prototype.addPrice = function() {
   var toppings = []
   for (i = 0; i < this.toppings.length; i++) {
-    toppings[i] = parseInt(this.toppings[i]);}
+    toppings[i] = parseInt(this.toppings[i]);
+  }
   var finalToppings = []
-    toppings.forEach(function(topping) {
-      if (topping === 2) {
-        finalToppings.push(topping)
-      }
-    });
+  toppings.forEach(function(topping) {
+    if (topping === 2) {
+      finalToppings.push(topping)
+    }
+  });
   var one = finalToppings.concat(this.size);
   var two = one.splice("");
   total = 0;
   two.forEach(function(number) {
-  total = number + total
-      });
+    total = number + total
+  });
 
-      return total
+  return total
 
-    };
-// total= 0
-// total = this.size + this.toppings.length;
-// return total
-// var one = this.size
-// var toppings = this.toppings.splice('')
-// total = 0;
-// toppings.forEach(function(topping){
-//       total = topping + total
-//     });
-// var three = one.concat(two);
-
-
-
-
-
-
-
-
-//   var size = []
-//   if (this.small === 10) {
-//     size.push(this.small)
-//   } else if (this.medium === 12) {
-//     size.push(this.medium)
-//   } else if (this.large === 14) {
-//     size.push(this.large)
-//   }
-//
-//   var toppings = []
-//   var finalToppings = []
-//   toppings.push(this.pepperoni, this.olives, this.sausage, this.peppers);
-//   toppings.forEach(function(topping) {
-//     if (topping === 2) {
-//       finalToppings.push(topping)
-//     }
-//   });
-//
-//
-
+};
 
 
 // UI Logic
